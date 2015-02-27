a97ee929826a238047df237f4d249cfdcbd5fd1b
Magnus Ulf <magnus@puj.dk>
2015-02-27 17:04:41 +0100

    New schedulers.
    Task was renamed to have a more verbose name.
    A tick independent self cancelling task was also implemented.

--- src/dk/muj/derius/lib/scheduler/RepeatingTask.java
+++ src/dk/muj/derius/lib/scheduler/RepeatingTask.java
@@ -1,24 +1,27 @@
-package dk.muj.derius.lib;
+package dk.muj.derius.lib.scheduler;
 
 import com.massivecraft.massivecore.ModuloRepeatTask;
 
 /**
- * @deprecated use RepeatTask
+ * This is our better version of ModuloRepeatTask
+ * that makes sure it will be executed at least X times per second
+ * even in lag intensive situations.
+ * This is especially useful for tasks being executed several times per second.
  */
-public abstract class Task extends ModuloRepeatTask
+public abstract class RepeatingTask extends ModuloRepeatTask
 {
 	// -------------------------------------------- //
 	// CONSTRUCT
 	// -------------------------------------------- //
 	
-	public Task(long delayMilis)
+	public RepeatingTask(long delayMilis)
 	{
 		this(delayMilis, System.currentTimeMillis());
 	}
 	
-	public Task(long delayMilis, long previousMillis)
+	public RepeatingTask(long delayMillis, long previousMillis)
 	{
-		this.setDelayMillis(delayMilis);
+		this.setDelayMillis(delayMillis);
 		this.setPreviousMillis(previousMillis);
 	}
 	
@@ -41,8 +44,6 @@ public abstract class Task extends ModuloRepeatTask
 			
 			this.setPreviousMillis(lastMillis);
 		}
-		
-		return;
 	}
 	
 }
---
+++ src/dk/muj/derius/lib/scheduler/TempTask.java
@@ -0,0 +1,77 @@
+package dk.muj.derius.lib.scheduler;
+
+import com.massivecraft.massivecore.ModuloRepeatTask;
+
+/**
+ * This is another version of RepeatingTask
+ * this will however only be
+ */
+public abstract class TempTask extends ModuloRepeatTask
+{
+	// -------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------- //
+	
+	private final int times;
+	public int getTimes() { return this.times; }
+	
+	private int invocation = 0;
+	public int getInvocation() { return this.invocation; }
+	public void setInvocation(int invocation) { this.invocation = invocation; }
+	
+	// -------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------- //
+	
+	public TempTask(long delayMilis, int times)
+	{
+		this(delayMilis, System.currentTimeMillis(), times);
+	}
+	
+	public TempTask(long delayMillis, long previousMillis, int times)
+	{
+		this.setDelayMillis(delayMillis);
+		this.setPreviousMillis(previousMillis);
+		this.times = times;
+	}
+	
+	// -------------------------------------------- //
+	// OVERRIDE
+	// -------------------------------------------- //
+	
+	@Override
+	public void run()
+	{
+		if (this.getDelayMillis() < 1) return;
+		
+		long nowMillis = System.currentTimeMillis();
+		
+		while (this.getPreviousMillis() + this.getDelayMillis() < nowMillis)
+		{
+			if (invocation++ < times)
+			{
+				this.deactivate();
+				return;
+			}
+			
+			long lastMillis = this.getPreviousMillis() + this.getDelayMillis();
+			
+			this.invoke(lastMillis);
+			
+			this.setPreviousMillis(lastMillis);
+		}
+		
+	}
+	
+	@Override
+	public void invoke(long l)
+	{
+		this.invoke();
+	}
+
+	// -------------------------------------------- //
+	// ABSTRACT
+	// -------------------------------------------- //
+	
+	public abstract void invoke();
+}
